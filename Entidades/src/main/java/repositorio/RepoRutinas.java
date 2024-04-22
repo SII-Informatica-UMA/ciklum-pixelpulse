89e28a748818033277681188ecae6d3ce7e04680
idolopafersa <pafersa2003@gmail.com>
2024-04-22 19:31:35 +0200

    repositorio rutinas

---
+++ Entidades/src/main/java/repositorio/RepoRutinas.java
@@ -0,0 +1,16 @@
+package repositorio;
+import java.util.List;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
+import pixelpulse.entidades.Rutina;
+
+@Repository
+
+public interface RepoRutinas extends JpaRepository<Rutina, Long> {
+    List<Rutina> findByIdEntrenador(Long idEntrenador);
+
+    @Query("SELECT CASE WHEN COUNT(r) > 0 THEN TRUE ELSE FALSE END FROM Rutina r WHERE EXISTS (SELECT 1 FROM r.ejercicios f WHERE f.ejercicio.id = :idEjercicio)")
+    boolean existsRutinaWithEjercicio(@Param("idEjercicio") Long idEjercicio);
+}
