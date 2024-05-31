ea59dc642f3aacff9b992f168f769265a6f99d1e
Joseluito71 <110116614+Joseluito71@users.noreply.github.com>
2024-05-31 10:32:48 +0200

    Avance en clase

--- Backend/src/test/java/pixelpulse/entidades/EntidadesApplicationTests.java
+++ Backend/src/test/java/pixelpulse/entidades/EntidadesApplicationTests.java
@@ -1,48 +1,54 @@
 package pixelpulse.entidades;
 
-import EntidadesApplication.Dtos.RutinaDTO;
-import EntidadesApplication.Dtos.RutinaNuevaDTO;
+import EntidadesApplication.Dtos.*;
 import EntidadesApplication.EntidadesApplication;
 import EntidadesApplication.entities.Rutina;
 import EntidadesApplication.repositories.EjerciciosRepo;
 import EntidadesApplication.repositories.RutinaRepo;
 
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.test.context.SpringBootTest;
-import EntidadesApplication.Dtos.EjercicioDTO;
-import EntidadesApplication.Dtos.EjercicioNuevoDTO;
 import EntidadesApplication.entities.Ejercicio;
 import EntidadesApplication.services.EjercicioService;
 import org.junit.jupiter.api.*;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.client.TestRestTemplate;
 import org.springframework.core.ParameterizedTypeReference;
-import org.springframework.http.MediaType;
-import org.springframework.http.RequestEntity;
-import org.springframework.http.ResponseEntity;
+import org.springframework.core.io.Resource;
+import org.springframework.http.*;
 import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.annotation.DirtiesContext;
+import org.springframework.test.web.client.ExpectedCount;
 import org.springframework.web.util.DefaultUriBuilderFactory;
 import org.springframework.web.util.UriBuilder;
 import org.springframework.web.util.UriBuilderFactory;
 import org.springframework.web.util.UriComponentsBuilder;
+import org.mockito.Mock;
 
 import java.net.URI;
-import java.util.HashMap;
+import java.net.URISyntaxException;
 import java.util.List;
 import java.util.Map;
+import org.springframework.test.web.client.MockRestServiceServer;
+import org.springframework.web.client.RestTemplate;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.springframework.test.web.client.match.MockRestRequestMatchers.*;
+import static org.springframework.test.web.client.response.MockRestResponseCreators.*;
 
+@ExtendWith(MockitoExtension.class)
 @SpringBootTest(classes = EntidadesApplication.class,webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 @DisplayName("En el servicio de Gestion y Ejercicios")
 @DirtiesContext(classMode = DirtiesContext.ClassMode.AFTER_EACH_TEST_METHOD)
 class EntidadesApplicationTests {
 
     @Autowired
-    private TestRestTemplate restTemplate;
-
+    private TestRestTemplate testRestTemplate;
+    @Mock
+    private RestTemplate restTemplate;
+    private MockRestServiceServer mockServer;
     @Value(value = "${local.server.port}")
     private int port;
     private String jwtToken;
@@ -57,6 +63,7 @@ class EntidadesApplicationTests {
 
     @BeforeEach
     public void initializeDatabase() {
+        mockServer=MockRestServiceServer.createServer(testRestTemplate.getRestTemplate());
         ejercicioRepository.deleteAll();
         rutinaRepository.deleteAll();
         jwtToken="eyJhbGciOiJIUzUxMiJ9.eyJzdWIiOiIxIiwiaWF0IjoxNzE1ODA4MjU2LCJleHAiOjYwMDAxNzE1ODA4MjU2fQ.TlGrRwC7BI0maP--cNOepyOGneYY9bTI4Zke6DTVw-T59FhS4QjjFN7NTm6GhcfxDmPbD2jcLdebzS8TXvEnbQ";
@@ -144,17 +151,32 @@ class EntidadesApplicationTests {
     @DisplayName("cuando no hay ejercicios")
     public class EjerciciosVacios {
 
+        @BeforeEach
+        public void setup() throws URISyntaxException {
+            String idEntrenadorValido="eyJhbGciOiJIUzUxMiJ9.eyJzdWIiOiI2MCIsImlhdCI6MTcxNzA5NDk5MywiZXhwIjoxNzE3MDk1NTkzfQ.VsEQvnbCMoakD06_Sm9_MRabUT4CHHYA6eSWydlWZuozNR6oOhpgzF6gonn-3ZB--AfsMfHwGJi6yr1pI1bw ";
+            EntrenadorDTO ed= new EntrenadorDTO(1L,1L,"123456789","Calle Falsa 123","12345678A",null,null,null,"Especialidad","Titulacion","Experiencia","Observaciones");
+            mockServer.expect(ExpectedCount.once(),
+                            requestTo(new URI("http://localhost:8080/entrenador/1")))
+                    .andExpect(method(HttpMethod.GET))
+                    .andRespond(withStatus(HttpStatus.OK)
+                            .contentType(MediaType.APPLICATION_JSON).body(String.valueOf(ed)));
+
+
+
+        }
+
         @Test
         @DisplayName("devuelve la lista de ejercicios vacía")
-        public void devuelveEjercicios() {
+        public void devuelveEjercicios() throws URISyntaxException {
+
             var peticion = get("http", "localhost", port, "/ejercicio");
 
-            var respuesta = restTemplate.exchange(peticion,
+            var respuesta = testRestTemplate.exchange(peticion,
                     new ParameterizedTypeReference<List<EjercicioDTO>>() {
                     });
-
             assertThat(respuesta.getStatusCode().value()).isEqualTo(200);
             assertThat(respuesta.getBody()).isEmpty();
+
         }
 
         @Test
@@ -163,7 +185,7 @@ class EntidadesApplicationTests {
             Long idEjercicio = 1L;
 
             var peticion = get("http", "localhost", port, "/ejercicio/" + idEjercicio);
-            var respuesta = restTemplate.exchange(peticion, Ejercicio.class);
+            var respuesta = testRestTemplate.exchange(peticion, Ejercicio.class);
 
             assertThat(respuesta.getStatusCode().value()).isEqualTo(404);
         }
@@ -177,7 +199,7 @@ class EntidadesApplicationTests {
 
             var peticion = put("http", "localhost", port, ejercicioActualizado, "/ejercicio/" + idEjercicio);
 
-            var respuesta = restTemplate.exchange(peticion, Ejercicio.class);
+            var respuesta = testRestTemplate.exchange(peticion, Ejercicio.class);
 
             assertThat(respuesta.getStatusCode().value()).isEqualTo(404);
         }
@@ -188,19 +210,19 @@ class EntidadesApplicationTests {
             Long idEjercicio = 1L;
 
             var peticion = delete("http", "localhost", port, "/ejercicio/" + idEjercicio);
-            var respuesta = restTemplate.exchange(peticion, Void.class);
+            var respuesta = testRestTemplate.exchange(peticion, Void.class);
 
             assertThat(respuesta.getStatusCode().value()).isEqualTo(404);
         }
 
     }
-
+    
     @Nested
     @DisplayName("cuando se crea un ejercicio")
     public class EjercicioEntrenador {
         public long idEj;
         @BeforeEach
-        public void setup() {
+        public void setup() throws URISyntaxException {
             ejercicioRepository.deleteAll();
             Ejercicio ejercicio = new Ejercicio();
             ejercicio.setNombre("Ejercicio 1");
@@ -208,6 +230,16 @@ class EntidadesApplicationTests {
             ejercicioRepository.save(ejercicio);
            List<Ejercicio> listaEj = ejercicioRepository.findAll();
             idEj = listaEj.get(0).getId();
+
+
+            String idEntrenadorValido="eyJhbGciOiJIUzUxMiJ9.eyJzdWIiOiI2MCIsImlhdCI6MTcxNzA5NDk5MywiZXhwIjoxNzE3MDk1NTkzfQ.VsEQvnbCMoakD06_Sm9_MRabUT4CHHYA6eSWydlWZuozNR6oOhpgzF6gonn-3ZB--AfsMfHwGJi6yr1pI1bw ";
+            EntrenadorDTO ed= new EntrenadorDTO(1L,1L,"123456789","Calle Falsa 123","12345678A",null,null,null,"Especialidad","Titulacion","Experiencia","Observaciones");
+            mockServer.expect(ExpectedCount.once(),
+                            requestTo(new URI("http://localhost:8080/entrenador/1")))
+                    .andExpect(method(HttpMethod.GET))
+                    .andRespond(withStatus(HttpStatus.OK)
+                            .contentType(MediaType.APPLICATION_JSON).body(String.valueOf(ed)));
+
 
         }
 
@@ -222,7 +254,7 @@ class EntidadesApplicationTests {
             nuevoEjercicio.setNombre("Ejercicio 2");
 
             var peticion = post("http", "localhost", port, nuevoEjercicio, "/ejercicio");
-            var respuesta = restTemplate.exchange(peticion, Ejercicio.class);
+            var respuesta = testRestTemplate.exchange(peticion, Ejercicio.class);
 
             assertThat(respuesta.getStatusCode().value()).isEqualTo(201);
             assertThat(respuesta.getBody().getNombre()).isEqualTo(nuevoEjercicio.getNombre());
@@ -231,11 +263,12 @@ class EntidadesApplicationTests {
         @Test
         @DisplayName("Obtiene todos los ejercicios a un entrenador")
         public void obtenerEjercicios() {
+            String idEntrenadorValido="eyJhbGciOiJIUzUxMiJ9.eyJzdWIiOiI2MCIsImlhdCI6MTcxNzA5NDk5MywiZXhwIjoxNzE3MDk1NTkzfQ.VsEQvnbCMoakD06_Sm9_MRabUT4CHHYA6eSWydlWZuozNR6oOhpgzF6gonn-3ZB--AfsMfHwGJi6yr1pI1bw ";
             var peticion = get("http", "localhost", port, "/ejercicio");
-            var respuesta = restTemplate.exchange(peticion,
+            var respuesta = testRestTemplate.exchange(peticion,
                     new ParameterizedTypeReference<List<Ejercicio>>() {
                     });
-
+            
             assertThat(respuesta.getStatusCode().value()).isEqualTo(200);
         }
     }
@@ -245,7 +278,7 @@ class EntidadesApplicationTests {
     public class EjerciciosEspecificos {
         public long idEj;
         @BeforeEach
-        public void setup() {
+        public void setup() throws URISyntaxException {
             ejercicioRepository.deleteAll();
             Ejercicio ejercicio = new Ejercicio();
             ejercicio.setNombre("Ejercicio 1");
@@ -253,6 +286,14 @@ class EntidadesApplicationTests {
             ejercicioRepository.save(ejercicio);
             List<Ejercicio> listaEj = ejercicioRepository.findAll();
             idEj = listaEj.get(0).getId();
+            String idEntrenadorValido="eyJhbGciOiJIUzUxMiJ9.eyJzdWIiOiI2MCIsImlhdCI6MTcxNzA5NDk5MywiZXhwIjoxNzE3MDk1NTkzfQ.VsEQvnbCMoakD06_Sm9_MRabUT4CHHYA6eSWydlWZuozNR6oOhpgzF6gonn-3ZB--AfsMfHwGJi6yr1pI1bw ";
+            EntrenadorDTO ed= new EntrenadorDTO(1L,1L,"123456789","Calle Falsa 123","12345678A",null,null,null,"Especialidad","Titulacion","Experiencia","Observaciones");
+            mockServer.expect(ExpectedCount.once(),
+                            requestTo(new URI("http://localhost:8080/entrenador/1")))
+                    .andExpect(method(HttpMethod.GET))
+                    .andRespond(withStatus(HttpStatus.OK)
+                            .contentType(MediaType.APPLICATION_JSON).body(String.valueOf(ed)));
+
         }
 
         @Test
@@ -261,7 +302,7 @@ class EntidadesApplicationTests {
             Long idEjercicio = idEj;
 
             var peticion = get("http", "localhost", port, "/ejercicio/" + idEjercicio);
-            var respuesta = restTemplate.exchange(peticion, Ejercicio.class);
+            var respuesta = testRestTemplate.exchange(peticion, Ejercicio.class);
 
             assertThat(respuesta.getStatusCode().value()).isEqualTo(200);
             assertThat(respuesta.getBody().getId()).isEqualTo(idEjercicio);
@@ -276,7 +317,7 @@ class EntidadesApplicationTests {
 
             var peticion = put("http", "localhost", port, ejercicioActualizado, "/ejercicio/" + idEjercicio);
 
-            var respuesta = restTemplate.exchange(peticion, Ejercicio.class);
+            var respuesta = testRestTemplate.exchange(peticion, Ejercicio.class);
 
             assertThat(respuesta.getStatusCode().value()).isEqualTo(200);
             assertThat(respuesta.getBody().getNombre()).isEqualTo(ejercicioActualizado.getNombre());
@@ -288,7 +329,7 @@ class EntidadesApplicationTests {
             Long idEjercicio = idEj;
 
             var peticion = delete("http", "localhost", port, "/ejercicio/" + idEjercicio);
-            var respuesta = restTemplate.exchange(peticion, Void.class);
+            var respuesta = testRestTemplate.exchange(peticion, Void.class);
 
             assertThat(respuesta.getStatusCode().value()).isEqualTo(200);
         }
@@ -304,7 +345,7 @@ class EntidadesApplicationTests {
         public void devuelveRutinas() {
             var peticion = get("http", "localhost", port, "/rutina");
 
-            var respuesta = restTemplate.exchange(peticion,
+            var respuesta = testRestTemplate.exchange(peticion,
                     new ParameterizedTypeReference<List<RutinaDTO>>() {
                     });
 
@@ -318,7 +359,7 @@ class EntidadesApplicationTests {
             Long idRutina = 1L;
 
             var peticion = delete("http", "localhost", port, "/rutina/" + idRutina);
-            var respuesta = restTemplate.exchange(peticion, Void.class);
+            var respuesta = testRestTemplate.exchange(peticion, Void.class);
 
             assertThat(respuesta.getStatusCode().value()).isEqualTo(404);
         }
@@ -329,7 +370,7 @@ class EntidadesApplicationTests {
             Long idRutina = 1L;
 
             var peticion = get("http", "localhost", port, "/rutina/" + idRutina);
-            var respuesta = restTemplate.exchange(peticion, Rutina.class);
+            var respuesta = testRestTemplate.exchange(peticion, Rutina.class);
 
             assertThat(respuesta.getStatusCode().value()).isEqualTo(404);
 
@@ -344,7 +385,7 @@ class EntidadesApplicationTests {
 
             var peticion = put("http", "localhost", port, rutinaActualizada, "/rutina/" + idRutina);
 
-            var respuesta = restTemplate.exchange(peticion, Rutina.class);
+            var respuesta = testRestTemplate.exchange(peticion, Rutina.class);
 
             assertThat(respuesta.getStatusCode().value()).isEqualTo(404);
         }
@@ -358,7 +399,7 @@ class EntidadesApplicationTests {
     public class RutinasEntrenador {
         public long idRut;
         @BeforeEach
-        public void setup() {
+        public void setup() throws URISyntaxException {
             rutinaRepository.deleteAll();
             Rutina rutina = new Rutina();
             rutina.setNombre("Rutina 1");
@@ -366,6 +407,15 @@ class EntidadesApplicationTests {
             rutinaRepository.save(rutina);
             List<Rutina> listaRut = rutinaRepository.findAll();
             idRut = listaRut.get(0).getId();
+
+            String idEntrenadorValido="eyJhbGciOiJIUzUxMiJ9.eyJzdWIiOiI2MCIsImlhdCI6MTcxNzA5NDk5MywiZXhwIjoxNzE3MDk1NTkzfQ.VsEQvnbCMoakD06_Sm9_MRabUT4CHHYA6eSWydlWZuozNR6oOhpgzF6gonn-3ZB--AfsMfHwGJi6yr1pI1bw ";
+            EntrenadorDTO ed= new EntrenadorDTO(1L,1L,"123456789","Calle Falsa 123","12345678A",null,null,null,"Especialidad","Titulacion","Experiencia","Observaciones");
+            mockServer.expect(ExpectedCount.once(),
+                            requestTo(new URI("http://localhost:8080/entrenador/1")))
+                    .andExpect(method(HttpMethod.GET))
+                    .andRespond(withStatus(HttpStatus.OK)
+                            .contentType(MediaType.APPLICATION_JSON).body(String.valueOf(ed)));
+
         }
 
         @Test
@@ -375,7 +425,7 @@ class EntidadesApplicationTests {
             rutina.setNombre("Rutina 2");
 
             var peticion = post("http", "localhost", port, rutina, "/rutina");
-            var respuesta = restTemplate.exchange(peticion, Rutina.class);
+            var respuesta = testRestTemplate.exchange(peticion, Rutina.class);
 
             assertThat(respuesta.getStatusCode().value()).isEqualTo(201);
             assertThat(respuesta.getBody().getNombre()).isEqualTo(rutina.getNombre());
@@ -385,7 +435,7 @@ class EntidadesApplicationTests {
         @DisplayName("devuelve la lista de rutinas de un entrenador")
         public void obtenerRutinas() {
             var peticion = get("http", "localhost", port, "/rutina");
-            var respuesta = restTemplate.exchange(peticion,
+            var respuesta = testRestTemplate.exchange(peticion,
                     new ParameterizedTypeReference<List<Rutina>>() {
                     });
             assertThat(respuesta.getStatusCode().value()).isEqualTo(200);
@@ -397,7 +447,7 @@ class EntidadesApplicationTests {
     public class RutinasEspecificas {
         public long idRut;
         @BeforeEach
-        public void setup() {
+        public void setup() throws URISyntaxException {
             rutinaRepository.deleteAll();
             Rutina rutina = new Rutina();
             rutina.setNombre("Rutina 1");
@@ -405,6 +455,15 @@ class EntidadesApplicationTests {
             rutinaRepository.save(rutina);
             List<Rutina> listaRut = rutinaRepository.findAll();
             idRut = listaRut.get(0).getId();
+
+            String idEntrenadorValido="eyJhbGciOiJIUzUxMiJ9.eyJzdWIiOiI2MCIsImlhdCI6MTcxNzA5NDk5MywiZXhwIjoxNzE3MDk1NTkzfQ.VsEQvnbCMoakD06_Sm9_MRabUT4CHHYA6eSWydlWZuozNR6oOhpgzF6gonn-3ZB--AfsMfHwGJi6yr1pI1bw ";
+            EntrenadorDTO ed= new EntrenadorDTO(1L,1L,"123456789","Calle Falsa 123","12345678A",null,null,null,"Especialidad","Titulacion","Experiencia","Observaciones");
+            mockServer.expect(ExpectedCount.once(),
+                            requestTo(new URI("http://localhost:8080/entrenador/1")))
+                    .andExpect(method(HttpMethod.GET))
+                    .andRespond(withStatus(HttpStatus.OK)
+                            .contentType(MediaType.APPLICATION_JSON).body(String.valueOf(ed)));
+
 
         }
 
@@ -414,7 +473,7 @@ class EntidadesApplicationTests {
             Long idRutina = idRut;
 
             var peticion = get("http", "localhost", port, "/rutina/" + idRutina);
-            var respuesta = restTemplate.exchange(peticion, Rutina.class);
+            var respuesta = testRestTemplate.exchange(peticion, Rutina.class);
 
             assertThat(respuesta.getStatusCode().value()).isEqualTo(200);
             assertThat(respuesta.getBody().getId()).isEqualTo(idRutina);
@@ -431,7 +490,7 @@ class EntidadesApplicationTests {
 
             var peticion = put("http", "localhost", port, rutinaActualizada, "/rutina/" + idRutina);
 
-            var respuesta = restTemplate.exchange(peticion, Rutina.class);
+            var respuesta = testRestTemplate.exchange(peticion, Rutina.class);
 
             assertThat(respuesta.getStatusCode().value()).isEqualTo(200);
             assertThat(respuesta.getBody().getNombre()).isEqualTo(rutinaActualizada.getNombre());
@@ -443,7 +502,7 @@ class EntidadesApplicationTests {
             Long idRutina = idRut;
 
             var peticion = delete("http", "localhost", port, "/rutina/" + idRutina);
-            var respuesta = restTemplate.exchange(peticion, Void.class);
+            var respuesta = testRestTemplate.exchange(peticion, Void.class);
 
             assertThat(respuesta.getStatusCode().value()).isEqualTo(200);
         }
